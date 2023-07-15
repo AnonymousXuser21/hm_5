d4e62fd172bc0f46c1ff76a626b623cd58ff4a6b
AnonymousXuser21 <anonymousxuserx21@gmail.com>
2023-07-15 11:49:06 -0700

    Initial commit

---
+++ src/Boss.java
@@ -0,0 +1,32 @@
+public class Boss {
+    private int health;
+    private int damage;
+    private String DefenceType;
+
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getDefenceType() {
+        return DefenceType;
+    }
+
+    public void setDefenceType(String defenceType) {
+        DefenceType = defenceType;
+    }
+
+
+}
---
+++ src/Hero.java
@@ -0,0 +1,22 @@
+public class Hero {
+    private int health;
+
+    private int damage;
+
+    private String superAbility;
+
+    public Hero(int health, int damage, String superAbility) {
+        this.health = health;
+        this.damage = damage;
+        this.superAbility = superAbility;
+    }
+
+    public Hero(int health, int damage) {
+        this.health = health;
+        this.damage = damage;
+    }
+
+
+
+
+}
